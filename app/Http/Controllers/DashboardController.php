3ea040e21f0ce709c6ff4fba9f2afcc464dea43a
AsepHidayatRamdani101 <asep.smkn8garut@gmail.com>
2026-04-02 21:12:07 +0700

    role

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,31 +9,32 @@ class DashboardController extends Controller
 {
     public function index()
     {
+        /** @var User $user */
+        $user = Auth::user();
 
-        if (auth()->user()->hasRole('super_admin')) {
+        if (!$user) {
+            abort(403);
+        }
+
+
+        if ($user->hasRole('super_admin')) {
             return view('dashboard.admin');
-        } else if (auth()->user()->hasRole('guru')) {
+        } else if ($user->hasRole('guru')) {
             return view('dashboard.guru');
-        } else if (auth()->user()->hasRole('siswa')) {
+        } else if ($user->hasRole('siswa')) {
             return view('dashboard.siswa');
-        } 
-         else if (auth()->user()->hasRole('kepala_tefa')) {
+        } else if ($user->hasRole('kepala_tefa')) {
             return view('dashboard.kepala_tefa');
-        }
-         else if (auth()->user()->hasRole('bendahara')) {
+        } else if ($user->hasRole('bendahara')) {
             return view('dashboard.bendahara');
-        }
-         else if (auth()->user()->hasRole('marketing')) {
+        } else if ($user->hasRole('marketing')) {
             return view('dashboard.marketing');
-        }
-         else if (auth()->user()->hasRole(roles: 'designer')) {
+        } else if ($user->hasRole(roles: 'designer')) {
             return view('dashboard.designer');
-        }
-         else if (auth()->user()->hasRole('produksi')) {
+        } else if ($user->hasRole('produksi')) {
             return view('dashboard.produksi');
-        }else {
+        } else {
             abort(403, 'Unauthorized');
         }
-
     }
 }
